fee08314ce08471278d4cc040f66790b6dcdc219
Ian Bibby <ian.bibby@gmail.com>
2017-04-01 17:42:22 -0400

    Get expenses for a given date

--- spec/unit/app/api_spec.rb
+++ spec/unit/app/api_spec.rb
@@ -7,6 +7,10 @@ module ExpenseTracker
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
     let(:app)    { API.new(ledger: ledger) }
 
+    def response_json
+      JSON.parse(last_response.body)
+    end
+
     describe 'POST /expenses' do
       context 'when the expense is successfully recorded' do
         let(:expense) { { 'some' => 'data' } }
@@ -21,8 +25,7 @@ module ExpenseTracker
         it 'returns the expense id' do
           post '/expenses', JSON.generate(expense)
 
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('expense_id' => 417)
+          expect(response_json).to include('expense_id' => 417)
         end
 
         it 'responds with a 200 (OK)' do
@@ -43,13 +46,50 @@ module ExpenseTracker
         it 'returns an error message' do
           post '/expenses', JSON.generate(expense)
 
-          parsed = JSON.parse(last_response.body)
-          expect(parsed).to include('error' => 'Expense incomplete')
+          expect(response_json).to include('error' => 'Expense incomplete')
         end
 
         it 'responds with a 422 (Unprocessable entity)' do
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(422)
+        end
+      end
+
+      describe 'GET /expenses/:date' do
+        context 'when expenses exist on the given date' do
+          before do
+            allow(ledger).to receive(:expenses_on)
+              .with('2017-04-01')
+              .and_return(['expense-1', 'expense-2'])
+          end
+
+          it 'returns the expense records as JSON' do
+            get '/expenses/2017-04-01'
+            expect(response_json).to eq(['expense-1', 'expense-2'])
+          end
+
+          it 'responds with a 200 (OK)' do
+            get '/expenses/2017-04-01'
+            expect(last_response.status).to eq(200)
+          end
+        end
+
+        context 'when there are no expenses on the given date' do
+          before do
+            allow(ledger).to receive(:expenses_on)
+              .with('2099-04-01')
+              .and_return([])
+          end
+
+          it 'returns an empty array as JSON' do
+            get '/expenses/2099-04-01'
+            expect(response_json).to eq([])
+          end
+
+          it 'responds with a 200 (OK)' do
+            get '/expenses/2099-04-01'
+            expect(last_response.status).to eq(200)
+          end
         end
       end
     end
